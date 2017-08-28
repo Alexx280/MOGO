623692218d0462a3e20f9dc70affeea828965bde
dldanil <dildanil@mail.ru>
2017-08-28 16:13:42 +0600

    блок

---
+++ src/app/story/story.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-story',
+  templateUrl: './app.story.html',
+  styleUrls: ['./story.css']
+})
+export class StoryComponent {
+  
+}
